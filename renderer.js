5f40e950d03b090ab94b4f4df910bf5aefc7c41b
David Yang <dayang@google.com>
2016-12-12 22:53:49 -0800

    Decided on using VUE.js. Simple example of JIRI UPDATE using VUE framework.

--- renderer.js
+++ renderer.js
@@ -1,77 +1,47 @@
-// This file is required by the index.html file and will
-// be executed in the renderer process for that window.
-// All of the Node.js APIs are available in this process.
-
-
-
 'use strict';
-console.log('here');
 const spawn = require( 'child_process' ).spawn;
+const Vue = require('./node_modules/vue/dist/vue.js');
 
+new Vue({
 
-var statusDiv = document.getElementById('status');
-var statusTextDiv = document.getElementById('status-text');
-var projectTable =  document.getElementById('project-table');
-getProjects();
+  el: '#jiri-update',
 
-// get list of projects
-function getProjects() {
-  var projects = [];
-  var ls = spawn( 'cd $FUCHSIA_WORKSPACE && jiri project list', {shell: true});
+  data: {
+    isUpdating: false,
+    isSuccessfulUpdate: false,
+    isFailedUpdated: false,
+    updateStatus: '',
+    updateError: '',
+  },
 
-  ls.stdout.on( 'data', data => {
-    // var projectInfo = parseProject(data);
-    var projectBits = `${data}`;
-    projectBits = projectBits.split('\n');
-    for (var i =0; i<projectBits.length; i++) {
-      projects.push(parseProject(projectBits[i]));
-    }
-  });
+  ready: function() {},
 
-  ls.stderr.on( 'close', code => {
-    if(code == 0) {
-      for (var i=0; i<projects.length; i++) {
-        var row = document.createElement('tr');
-        row.innerHTML = '<td>' + projects[i].name + '</td><td>' +  projects[i].remote + '</td>'
-        projectTable.appendChild(row)
-      }
-    }
-  });
-}
+  methods: {
+    // Runs a Jiri Update command and updates the UI accordingly
+    handleJiriUpdate: function() {
+      // Update state
+      this.isUpdating = true;
+      this.isSuccessfulUpdate = false;
+      this.isFailedUpdate = false;
+      this.updateStatus = '';
+      this.updateError = '';
 
-function parseProject(project) {
-  console.log(project);
-  var projectInfo = project.split(' ');
-  var result = {};
-  for (var i=0; i<projectInfo.length; i++) {
-    var kv = projectInfo[i].split('=');
-    if(kv.length == 2) {
-      result[kv[0]] = kv[1].substring(1,kv[1].length-1);
+      var ls = spawn( 'cd $FUCHSIA_WORKSPACE && jiri update', {shell: true});
+
+      ls.stdout.on( 'data', data => {
+        this.updateStatus += data + '\n';
+      });
+
+      ls.stderr.on( 'data', data => {
+        this.isUpdating = false;
+        this.isFailedUpdate = true
+        this.updateError = data;
+      });
+
+      ls.on( 'close', code => {
+        this.isUpdating = false;
+        this.isSuccessfulUpdate = true;
+      });
     }
   }
-  return result;
-}
-
-document.getElementById('jiri-update-button').addEventListener("click", function() {
-  var ls = spawn( 'cd $FUCHSIA_WORKSPACE && jiri update', {shell: true});
-
-  statusDiv.innerHTML = 'DOING WORK';
-
-  ls.stdout.on( 'data', data => {
-    console.log( `stdout: ${data}` );
-    statusTextDiv.innerHTML += '<br/> ' + data;
-  });
-
-  ls.stderr.on( 'data', data => {
-    console.log( `stderr: ${data}` );
-  });
-
-  ls.on( 'close', code => {
-    console.log( `child process exited with code ${code}` );
-    if(code == 0) {
-      console.log('Done, like a boss!');
-      statusDiv.innerHTML = 'UPDATE COMPLETE...LIKE A BOSS';
-      statusTextDiv.innerHTML = '';
-    }
-  });
 });
